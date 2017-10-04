5acae1c8fbed2eca0644b641009bbdcad7c09a46
Fabio Maienschein <fmaiensc@lakeheadu.ca>
2017-10-04 19:23:53 -0400

    Prototypical implementation of landing and login page.

--- MusicVision/MusicVision/Controllers/HomeController.cs
+++ MusicVision/MusicVision/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace MusicVision.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
