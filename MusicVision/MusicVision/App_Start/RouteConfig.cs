a581c2b7322437e28f97db047564b5c02312d302
Fabio Maienschein <fmaiensc@lakeheadu.ca>
2017-10-14 20:40:38 -0400

    First attempt of Spotify Authorization. Introduce feature to get current playing song. Add vue.js as a client-side js framework.

--- MusicVision/MusicVision/App_Start/RouteConfig.cs
+++ MusicVision/MusicVision/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace MusicVision
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "Callback",                                          
+                "callback",                                           
+                new { controller = "Home", action = "MusicLounge" }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- MusicVision/MusicVision/Controllers/HomeController.cs
+++ MusicVision/MusicVision/Controllers/HomeController.cs
@@ -13,23 +13,9 @@ namespace MusicVision.Controllers
             return View();
         }
 
-        public ActionResult Login()
+        public ActionResult MusicLounge()
         {
             return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        }        
     }
 }
